707d70cce50eb76d867ebcd1e74a8c60f03f005e
Kyle Lynch <kylelynch104@gmail.com>
2019-06-19 17:21:45 -0600

    version_0.1.0

---
+++ src/components/users/Search.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Search extends Component {
+  state = {
+    text: ''
+  };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: '' });
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    const { showClear, clearUsers } = this.props;
+
+    return (
+      <div>
+        <form onSubmit={this.onSubmit} className="form">
+          <input
+            value={this.state.text}
+            onChange={this.onChange}
+            type="text"
+            name="text"
+            placeholder="Search Users.."
+          />
+          <input
+            type="submit"
+            value="search"
+            className="btn btn-block btn-dark"
+          />
+        </form>
+        {showClear && (
+          <button className="btn btn-block btn-light" onClick={clearUsers}>
+            Clear
+          </button>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Search;
